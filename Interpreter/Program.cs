2a01da014afddd8d04c16722be8ec9f3c86d9537
Michał Dela <dela_m@interia.pl>
2019-04-30 18:07:15 +0200

    Added working interpreter

--- Interpreter/Program.cs
+++ Interpreter/Program.cs
@@ -1,15 +1,38 @@
-﻿using System;
+﻿using Interpreter.TerminalExpressions;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Interpreter
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            string roman = "MCMXXVIII";
+            Context context = new Context(roman);
+
+            // Build the 'parse tree'
+
+            List<Expression> tree = new List<Expression>();
+            tree.Add(new ThousandExpression());
+            tree.Add(new HundredExpression());
+            tree.Add(new TenExpression());
+            tree.Add(new OneExpression());
+
+            // Interpret
+
+            foreach (Expression exp in tree)
+            {
+                exp.Interpret(context);
+            }
+
+            Console.WriteLine("{0} = {1}",
+              roman, context.Output);
+
+            // Wait for user
+
+            Console.ReadKey();
         }
+
     }
 }
